21d086234cb4d786d14ca04cc4f6500424d58062
ruler <epyruskdh@gmail.com>
2024-05-27 14:29:27 +0900

    [add] OAuth 쿠키 설정 추가

--- src/main/java/com/dongpani/springbootdeveloper/domain/User.java
+++ src/main/java/com/dongpani/springbootdeveloper/domain/User.java
@@ -18,6 +18,10 @@ import java.util.List;
 @Entity
 public class User implements UserDetails {
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
@@ -30,9 +34,16 @@ public class User implements UserDetails {
     private String password;
 
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
+    }
+
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+        return this;
     }
 
     @Override
